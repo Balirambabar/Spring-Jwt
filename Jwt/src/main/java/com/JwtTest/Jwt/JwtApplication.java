293011e0e5fbdec6144219e17f4bd147894acb9a
Baliram <balirambabar@gmail.com>
2023-09-21 14:47:13 +0530

    initial commit Jwt

---
+++ Jwt/src/main/java/com/JwtTest/Jwt/JwtApplication.java
@@ -0,0 +1,16 @@
+package com.JwtTest.Jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+	}
+
+}
+
+
